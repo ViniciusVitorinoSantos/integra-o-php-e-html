bd5964e0a6ef04504e0da275057c6459ab9165dd
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2024-08-08 22:09:00 -0300

    first commit

---
+++ dados.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta name="author" content="Vinícius Vitorino"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<?php
+
+?>
+</body>
+
+</html>
