62ebdfc9b5d7137d5e203bbfe182e8a0e66445ff
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2024-08-08 22:09:49 -0300

    add dados.php

--- dados.php
+++ dados.php
@@ -5,13 +5,23 @@
     <meta name="author" content="Vinícius Vitorino"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="_css/estilo.css" type="text/css">
+    <title>Pegando dados do HTML com PHP</title>
 </head>
 
 <body>
-<?php
+<div>
+    <form method="get" action="dados.php">
+        Valor:<input type="text" name="v">
+        <input type="submit" value="Calcular">
 
+    </form>
+<?php
+$valor = $_GET["v"];
+$resultado = sqrt($valor);
+echo $resultado;
 ?>
 </body>
+</div>
 
 </html>
